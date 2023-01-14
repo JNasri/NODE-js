4204d00444be45fde4c9dea7bf769f383b277f4d
JNasri <91850229+JNasri@users.noreply.github.com>
2023-01-14 23:33:05 +0300

    Intro to Express.js

--- server.js
+++ server.js
@@ -1,163 +1,85 @@
-/* THE EMITTER PART*/
-// require the logEvents we did
-const logEvents = require("./logEvents");
-// EventEmitter clas
-const EventEmitter = require("events");
-const { request } = require("http");
-class MyEmitter extends EventEmitter {}
-// initialize object
-const myEmitter = new MyEmitter();
-
-// add listener for the log events
-// with message and filename for each myEmitter.emit(...) call
-myEmitter.on("log", (msg, filename) => logEvents(msg, filename));
-
-//
-//
-
-/* THE SERVER PART*/
-const http = require("http");
+// require the framework 'express'
+const express = require("express");
+// make the app (server) , so instead of 'server' we use 'app'
+const app = express();
 const path = require("path");
-const fs = require("fs");
-const fsP = require("fs").promises;
 
 // fisrt we need to define a port for our web server,
 // the port we are using for Node/Nodemon is for dev
 const PORT = process.env.PORT || 3500;
 
-//Note: this function is for the server, needed later
-// need filepath, contentType and response object (res)
-const serveFile = async (filePath, contentType, response) => {
-  try {
-    // get the file
-    const rawData = await fsP.readFile(
-      filePath,
-      // check if data is image so no need for utf8
-      !contentType.includes("image") ? "utf8" : ""
-    );
+//
+// in express we use : app.get , app.post ....etc
+// express handles all contentType issues we had in old-server
+// 404(bad) or 200(good) is also handled automatically
+// express handles these like a waterfall, so you can put-
+// the default app.get at the end of them all
 
-    // if its json, go ahead and parse it
-    const data =
-      contentType === "application/json" // if json
-        ? JSON.parse(rawData) // parse
-        : rawData; // else leave it alone
+// express takes regExp as the route so you can specify
+// ^/$ ==only / OR index(.html)? ?== 0 or 1
+app.get("^/$|/index(.html)?", (req, res) => {
+  // these are two ways to call files, all works the same
+  // res.sendFile("./views/index.html", { root: __dirname });
+  res.sendFile(path.join(__dirname, "views", "index.html"));
+});
 
-    //now , check if this func was called by 200 or 404
-    // then writeHead that statusCode
-    response.writeHead(filePath.includes("404.html") ? 404 : 200, {
-      contectType: contentType,
-    });
+// define new page
+app.get("/new-page(.html)?", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "new-page.html"));
+});
 
-    // send the file
-    response.end(
-      // again, check if json to make it string
-      contentType === "application/json" // if json
-        ? JSON.stringify(data) // stringify
-        : data // else leave it alone
-    );
-  } catch (error) {
-    // FOR TESTING
-    console.log(error);
-    //emit the error request
-    myEmitter.emit("log", `${error.name} : ${error.message} \n`, "errLog.txt");
-    response.statusCode = 500; // 500 = server error
-    response.end();
+// now lets try to redirect the old page
+// to the new page like we did in old-server
+app.get("/old-page(.html)?", (req, res) => {
+  // 302 be default , but we make it 301 to tell search engines
+  // to know that old page has moved to new page
+  res.redirect(301, "/new-page.html");
+});
+
+//
+// Nested Route
+//
+
+// nested route handlers , we can see a new parameter 'next'
+// which we execute at the end to start the next route
+app.get(
+  "/hello(.html)?",
+  (req, res, next) => {
+    console.log("attempted to load hello.html");
+    next();
+  },
+  (req, res) => {
+    res.send("Hello World!");
   }
+);
+
+// another way is to define the methods befor like this:
+const one = (req, res, next) => {
+  console.log("one");
+  next();
 };
+const two = (req, res, next) => {
+  console.log("two");
+  next();
+};
+const three = (req, res) => {
+  console.log("three");
+  res.send("finished!!");
+};
+// then call them like array as parameter
+app.get("/chain(.html)?", [one, two, three]);
 
-// now we create a server using the http module
-// the function will get a req and res (request, response)
-// req: what user requests -- res: respond form server
-const server = http.createServer((req, res) => {
-  // FOR TESTING
-  // console.log(req.url, req.method);
-
-  //emit the request with the url and method to a file
-  myEmitter.emit("log", `${req.url}\t${req.method} \n`, "reqLog.txt");
-
-  // (1) we need to know the req in order to give the propre res
-  //
-  //
-
-  // first, check for the file extension (html,css,png...)
-  const extension = path.extname(req.url);
-  // variable for the contentType header
-  let contentType;
-  // check the type and give content type
-  switch (extension) {
-    case ".css":
-      contentType = "text/css";
-      break;
-    case ".js":
-      contentType = "text/javascript";
-      break;
-    case ".json":
-      contentType = "application/json";
-      break;
-    case ".png":
-      contentType = "image/png";
-      break;
-    case ".jpg":
-      contentType = "image/jpeg";
-      break;
-    case ".txt":
-      contentType = "text/plain";
-      break;
-    default:
-      // .html
-      contentType = "text/html";
-      break;
-  }
-  // now after we found the contentType, we find the specific path
-  let filePath =
-    contentType === "text/html" && req.url === "/" // if type html and req is just /
-      ? path.join(__dirname, "views", "index.html") // res = index.html
-      : contentType === "text/html" && req.url.slice(-1) === "/" // if it's subdir
-      ? path.join(__dirname, "views", req.url, "index.html") // res = /subdir/index.ht
-      : contentType === "text/html" // if req is other html file
-      ? path.join(__dirname, "views", req.url) // res = path to that file
-      : path.join(__dirname, req.url); //(LAST ELSE) if any other contentType is req
-  // here , if the request has no extention at all, we add it
-  // this makes html extension not required in the browser
-  if (!extension && req.url.slice(-1) != "/") {
-    filePath += ".html";
-  }
-
-  // (1) we need to know the req in order to give the propre res
-  // (2) we have content type and filePath, we are ready to response
-  //
-
-  // we check if what required actually exists
-  const fileExists = fs.existsSync(filePath); // returns T or F
-  if (fileExists) {
-    // serve the file
-    serveFile(filePath, contentType, res);
-  } else {
-    // maybe 301 : redirect (if page requested in old....etc)
-    // maybe 404 : page/file not found
-
-    // we only need the base from the filepath (xxx.html)
-    switch (path.parse(filePath).base) {
-      case "old-page.html":
-        // if req is an old page, redirect to new page
-        res.writeHead(301, { Location: "/new-page.html" });
-        res.end();
-        break;
-      case "www-page.html":
-        // if req is old index page, redirect to new page
-        res.writeHead(301, { Location: "/" });
-        res.end();
-        break;
-      default:
-        // if page is not found, serve a 404 res
-        // notice here the file path has 404.html in
-        serveFile(path.join(__dirname, "views", "404.html"), "text/html", res);
-    }
-  }
+//
+// at the end, any other requests will go to 404
+// * == all
+app.get("/*", (req, res) => {
+  // we need to manually send 404 status code
+  // because it will find 404.html and send 200 which we do not want
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 // server needs to listen for a request to be given
 // when a request happen at the PORT number, it will run http.createServer
 // which is the function written above
 // note: this should be always at the end of server.js file
-server.listen(PORT, () => console.log(`server running on port : ${PORT}`));
+app.listen(PORT, () => console.log(`server running on port : ${PORT}`));
